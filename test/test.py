912047c94591520d6bdca7b9efe661840a3a3dca
Disa Mhembere <dmhembe1@jhu.edu>
2017-07-13 14:23:45 -0400

    test: data packaged with python

--- test/test.py
+++ test/test.py
@@ -22,7 +22,7 @@
 from knor import knor
 import numpy as np
 
-FN = "../../test-data/matrix_r50_c5_rrw.bin"
+FN = "../data/matrix_r50_c5_rrw.bin"
 
 def dim_test_c_comp():
     data = np.random.random((10,3))
@@ -47,13 +47,13 @@ def test_err():
     except Exception, msg:
         print "Successful test: CORRECTLY fails with message: {}".format(msg)
 
-print "\n\n***************TEST******************\n\n"
+print "\n\n*************** TEST 1/5 ******************\n\n"
 print dim_test_c_comp()
-print "\n\n***************TEST******************\n\n"
+print "\n\n*************** TEST 2/5 ******************\n\n"
 print dim_test_c_im()
-print "\n\n***************TEST******************\n\n"
+print "\n\n*************** TEST 3/5 ******************\n\n"
 print dexm_test_c_comp()
-print "\n\n***************TEST******************\n\n"
+print "\n\n*************** TEST 4/5 ******************\n\n"
 print dexm_test_c_im()
-print "\n\n***************TEST******************\n\n"
+print "\n\n*************** TEST 5/5 ******************\n\n"
 test_err()
