22945952a97042f7501e03b5b417c414586d7840
Disa Mhembere <dmhembe1@jhu.edu>
2017-07-17 05:28:12 -0400

    bundling numpy headers with package

--- setup.py
+++ setup.py
@@ -35,22 +35,21 @@ if OS not in _OS_SUPPORTED_.values():
 
 # Hack to stop -Wstrict-prototypes warning on linux
 if OS == _OS_SUPPORTED_["linux"]:
-    from distutils.sysconfig import get_config_vars
+    from distutils import sysconfig
 
-    (opt,) = get_config_vars('OPT')
-    os.environ['OPT'] = " ".join(
-                flag for flag in opt.split() if flag != "-Wstrict-prototypes"
-                )
+    sysconfig._config_vars["OPT"] = \
+            sysconfig._config_vars["OPT"].replace("-Wstrict-prototypes", "")
+    sysconfig._config_vars["OPT"] = \
+            sysconfig._config_vars["OPT"].replace("-O2", "-O3")
+    (os.environ["OPT"],) = sysconfig.get_config_vars("OPT")
 
-    os.environ['OPT'] = os.environ['OPT'].replace("-DNDEBUG", "")
-    os.environ['OPT'] = os.environ['OPT'].replace("-O2", "-O3")
 ################################ End VarDecl ###################################
 
 # For C++ libraries
 libkcommon = ("kcommon",
         {"sources": glob(os.path.join("knor", "cknor", "libkcommon", "*.cpp"))})
-libman = ('man',
-        {'sources': glob(os.path.join("knor", "cknor", "libman", "*.cpp"))})
+libman = ("man",
+        {"sources": glob(os.path.join("knor", "cknor", "libman", "*.cpp"))})
 
 # Minimum libraries we will build
 libraries = [libkcommon, libman]
@@ -66,13 +65,12 @@ sources.append(os.path.join("knor", "knor.pyx"))
 
 # Compile & link arguments
 if OS == _OS_SUPPORTED_["linux"]:
-    extra_compile_args = ["-std=c++11", "-O3", "-fPIC",
+    extra_compile_args = ["-std=c++11", "-fPIC",
             "-Wno-attributes", "-Wno-unused-variable",
             "-Wno-unused-function", "-I.","-Iknor/cknor/libman",
             "-Iknor/cknor/libauto",
             "-Iknor/cknor/binding", "-Iknor/cknor/libkcommon",
             "-fopenmp",
-            "-I/usr/local/lib/python2.7/dist-packages/numpy/core/include",
             "-I/usr/include/python2.7",
             "-DBIND", "-DLINUX"]
 
@@ -83,10 +81,9 @@ if OS == _OS_SUPPORTED_["linux"]:
             ]
 
 elif OS == _OS_SUPPORTED_["mac"]:
-    extra_compile_args = ["-std=c++11", "-O3",
+    extra_compile_args = ["-std=c++11",
             "-Wno-unused-function", "-I.","-Iknor/cknor/libman",
             "-Iknor/cknor/binding", "-Iknor/cknor/libkcommon",
-            "-I/usr/local/lib/python2.7/site-packages/numpy/core/include",
             "-DBIND", "-DOSX"]
 
     extra_link_args=["-Lknor/cknor/libman", "-lman",
@@ -120,7 +117,6 @@ class knor_clib(build_clib, object):
         elif OS == _OS_SUPPORTED_["mac"]:
             self.include_dirs = [
                 "knor/cknor/libman", "knor/cknor/binding", "knor/cknor/libkcommon",
-                "/usr/local/lib/python2.7/site-packages/numpy/core/include",
                 ]
 
             self.define = [
@@ -132,24 +128,24 @@ class knor_clib(build_clib, object):
 
     def build_libraries(self, libraries):
         for (lib_name, build_info) in libraries:
-            sources = build_info.get('sources')
+            sources = build_info.get("sources")
             if sources is None or not isinstance(sources, (list, tuple)):
                 raise DistutilsSetupError, \
-                      ("in 'libraries' option (library '%s'), " +
-                       "'sources' must be present and must be " +
+                      ("in \"libraries\" option (library \"%s\"), " +
+                       "\"sources\" must be present and must be " +
                        "a list of source filenames") % lib_name
             sources = list(sources)
 
-            print "building '%s' library" % lib_name
+            print "building \"%s\" library" % lib_name
 
             # First, compile the source code to object files in the library
             # directory.  (This should probably change to putting object
             # files in a temporary build directory.)
-            macros = build_info.get('macros')
-            include_dirs = build_info.get('include_dirs')
+            macros = build_info.get("macros")
+            include_dirs = build_info.get("include_dirs")
 
             # pass flasgs to compiler
-            extra_preargs = ["-std=c++11", "-O3", "-Wno-unused-function"]
+            extra_preargs = ["-std=c++11", "-Wno-unused-function"]
 
             if OS == _OS_SUPPORTED_["linux"]:
                 extra_preargs.append("-fopenmp")
@@ -162,7 +158,7 @@ class knor_clib(build_clib, object):
                                             extra_preargs=extra_preargs)
 
             # Now "link" the object files together into a static library.
-            # (On Unix at least, this isn't really linking -- it just
+            # (On Unix at least, this isn"t really linking -- it just
             # builds an archive.  Whatever.)
             self.compiler.create_static_lib(objects, lib_name,
                                             output_dir=self.build_clib,
@@ -173,7 +169,7 @@ class knor_clib(build_clib, object):
 # Run the setup
 setup(
     name="knor",
-    version="0.0.1a11",
+    version="0.0.1a12",
     description="A fast parallel k-means library for Linux and Mac",
     long_description="The k-means NUMA Optimized Routine library or " +\
     "knor is a highly optimized and fast library for computing " +\
@@ -192,6 +188,6 @@ setup(
     package_dir = {"knor": "knor"},
     packages=["knor", "knor.Exceptions"],
     libraries =libraries,
-    cmdclass = {'build_clib': knor_clib, 'build_ext': build_ext},
+    cmdclass = {"build_clib": knor_clib, "build_ext": build_ext},
     ext_modules = ext_modules,
     )
