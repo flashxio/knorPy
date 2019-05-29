990710c0e05d1d453630969356110c7f0623ae98
Disa Mhembere <dmhembe1@jhu.edu>
2019-05-28 23:08:08 -0500

    added fuzzy c means

--- knor/__init__.py
+++ knor/__init__.py
@@ -4,8 +4,12 @@ PYTHON_VERSION = sys.version_info[0]
 if PYTHON_VERSION == 2:
     from knor import Kmeans
     from knor import KmeansPP
+    from knor import SKmeans
+    from knor import FuzzyCMeans
     from knor import cluster_t
 else:
     from .knor import Kmeans
     from .knor import KmeansPP
+    from .knor import SKmeans
+    from .knor import FuzzyCMeans
     from .knor import cluster_t
--- knor/knor.cpp
+++ knor/knor.cpp
@@ -5,9 +5,8 @@
 #include "cknor/libkcommon/kcommon.hpp"
 #include "cknor/binding/kmeanspp.hpp"
 #include "cknor/libman/kmeans_task_coordinator.hpp"
-//#include "cknor/libman/kmeans_coordinator.hpp"
-//#include "cknor/libman/skmeans_coordinator.hpp"
-//#include "cknor/libman/fcm_coordinator.hpp"
+#include "cknor/libman/skmeans_coordinator.hpp"
+#include "cknor/libman/fcm_coordinator.hpp"
 //#include "cknor/libman/medoid_coordinator.hpp"
 //#include "cknor/libman/hclust_coordinator.hpp"
 //#include "cknor/libman/xmeans_coordinator.hpp"
@@ -72,6 +71,66 @@ class KmeansPP {
         }
 };
 
+class SKmeans {
+    public:
+        kbase::cluster_t fit(double* data, const size_t nrow,
+                const size_t ncol, const unsigned k,
+                size_t max_iters=std::numeric_limits<size_t>::max(),
+                unsigned nnodes=kbase::get_num_nodes(),
+                unsigned nthread=kbase::get_num_omp_threads(),
+                double* p_centers=NULL, std::string init="kmeanspp",
+                double tolerance=-1, std::string dist_type="cos") {
+
+            return knor::skmeans_coordinator::create(
+                    "", nrow, ncol, k, max_iters, nnodes,
+                    nthread, p_centers, init, tolerance,
+                    dist_type)->run(&data[0]);
+        }
+
+        kbase::cluster_t fit(const std::string datafn, const size_t nrow,
+                const size_t ncol, const unsigned k,
+                size_t max_iters=std::numeric_limits<size_t>::max(),
+                unsigned nnodes=kbase::get_num_nodes(),
+                unsigned nthread=kbase::get_num_omp_threads(),
+                double* p_centers=NULL, std::string init="kmeanspp",
+                double tolerance=-1, std::string dist_type="cos") {
+
+            return knor::skmeans_coordinator::create(
+                    datafn, nrow, ncol, k, max_iters, nnodes,
+                    nthread, p_centers, init, tolerance, dist_type)->run();
+        }
+};
+
+class FuzzyCMeans {
+    public:
+        kbase::cluster_t fit(double* data, const size_t nrow,
+                const size_t ncol, const unsigned k, const unsigned max_iters,
+                const unsigned nthread,
+                const double* p_centers=NULL, const std::string init="forgy",
+                const double tolerance=-1, const std::string dist_type="cos",
+                const unsigned fuzzindex=2) {
+
+
+            return knor::fcm_coordinator::create(
+                    "", nrow, ncol, k, max_iters, kbase::get_num_nodes(),
+                    nthread, p_centers, init, tolerance,
+                    dist_type, fuzzindex)->run(&data[0]);
+        }
+
+        kbase::cluster_t fit(const std::string fn, const size_t nrow,
+                const size_t ncol, const unsigned k, const unsigned max_iters,
+                const unsigned nthread,
+                const double* p_centers=NULL, const std::string init="forgy",
+                const double tolerance=-1, const std::string dist_type="cos",
+                const unsigned fuzzindex=2) {
+
+            return knor::fcm_coordinator::create(
+                    fn, nrow, ncol, k, max_iters, kbase::get_num_nodes(),
+                    nthread, p_centers, init, tolerance,
+                    dist_type, fuzzindex)->run();
+        }
+};
+
 
 PYBIND11_MODULE(knor, m) {
     m.doc() = R"pbdoc(
@@ -115,6 +174,36 @@ PYBIND11_MODULE(knor, m) {
                             const unsigned, const unsigned,
                 unsigned nthread, std::string)) &KmeansPP::fit,
                     "Compute kmeans++ on the dataset provided");
+
+    // SKmeans
+    py::class_<SKmeans>(m, "SKmeans")
+            .def(py::init(), "Create a Kmeans object")
+            .def("fit", (kbase::cluster_t (SKmeans::*)(double*, const size_t,
+                            const size_t, const unsigned, size_t, unsigned,
+                            unsigned, double*, std::string, double, std::string
+                            )) &SKmeans::fit,
+                    "Compute kmeans on the dataset provided")
+            .def("fit", (kbase::cluster_t (SKmeans::*)(const std::string,
+                            const size_t, const size_t, const unsigned,
+                            size_t, unsigned, unsigned, double*, std::string,
+                            double, std::string)) &SKmeans::fit,
+                    "Compute kmeans on the dataset provided");
+
+    // FuzzyCMeans
+    py::class_<FuzzyCMeans>(m, "FuzzyCMeans")
+            .def(py::init(), "Create a Kmeans object")
+            .def("fit", (kbase::cluster_t (FuzzyCMeans::*)(double*,
+                            const size_t, const size_t, const unsigned,
+                            const unsigned, const unsigned, const double*,
+                            const std::string, const double, const std::string,
+                            const unsigned)) &FuzzyCMeans::fit,
+                    "Compute kmeans on the dataset provided")
+            .def("fit", (kbase::cluster_t (FuzzyCMeans::*)(
+                            const std::string, const size_t, const size_t,
+                            const unsigned, const unsigned, const unsigned,
+                            const double*, const std::string, const double,
+                            const std::string, const unsigned)) &FuzzyCMeans::fit,
+                    "Compute kmeans on the dataset provided");
 
     // Versioning information
 #ifdef VERSION_INFO
