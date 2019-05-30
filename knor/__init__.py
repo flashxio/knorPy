1d7037ccebb46c1e5ec130ecf8c6605ea2419c84
Disa Mhembere <dmhembe1@jhu.edu>
2019-05-30 02:58:48 -0400

    expose all functions

--- knor/__init__.py
+++ knor/__init__.py
@@ -6,10 +6,18 @@ if PYTHON_VERSION == 2:
     from knor import KmeansPP
     from knor import SKmeans
     from knor import FuzzyCMeans
+    from knor import Kmedoids
+    from knor import Hmeans
+    from knor import Xmeans
+    from knor import Gmeans
     from knor import cluster_t
 else:
     from .knor import Kmeans
     from .knor import KmeansPP
     from .knor import SKmeans
     from .knor import FuzzyCMeans
+    from .knor import Kmedoids
+    from .knor import Hmeans
+    from .knor import Xmeans
+    from .knor import Gmeans
     from .knor import cluster_t
