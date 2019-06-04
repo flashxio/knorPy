c598785331408fa27e4a8b5bd22bc631a31b61fa
Disa Mhembere <dmhembe1@jhu.edu>
2019-06-04 11:38:18 -0400

    kmeans test updated for new api

--- test/test.py
+++ test/test.py
@@ -17,18 +17,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Note these tests only work from the knor repo
+# Note these tests only work from the knorPy/test directory
 
 import sys
 
-PYTHON_VERSION = sys.version_info[0]
-if PYTHON_VERSION == 2:
-    COMMON_INSTALL_LOC = "/usr/local/lib/python2.7/dist-packages"
-elif PYTHON_VERSION == 3:
-    COMMON_INSTALL_LOC = "/usr/local/lib/python3.5/dist-packages"
+# PYTHON_VERSION = sys.version_info[0]
+# if PYTHON_VERSION == 2:
+    # COMMON_INSTALL_LOC = "/usr/local/lib/python2.7/dist-packages"
+# elif PYTHON_VERSION == 3:
+    # COMMON_INSTALL_LOC = "/usr/local/lib/python3.5/dist-packages"
 
-if COMMON_INSTALL_LOC not in sys.path:
-    sys.path.append(COMMON_INSTALL_LOC)
+# if COMMON_INSTALL_LOC not in sys.path:
+    # sys.path.append(COMMON_INSTALL_LOC)
 
 import knor
 import numpy as np
@@ -37,24 +37,30 @@ FN = "../data/matrix_r50_c5_rrw.bin"
 
 def dim_test_c_comp():
     data = np.random.random((10,3))
-    return knor.Kmeans(data, 4)
+    km = knor.Kmeans(4)
+    return km.fit(data)
 
 def dim_test_c_im():
     data = np.random.random((10,3))
-    centers = np.random.random((3,3))
-    return knor.Kmeans(data, centers)
+    centers = np.random.random((3,3)).flatten() # TODO: permit 2D ndarray
+    km = knor.Kmeans(3, centers=centers) # TODO: Infer k
+    return km.fit(data)
+
 
 def dexm_test_c_comp():
-    centers = 8
-    return knor.Kmeans(FN, centers, nrow=50, ncol=5)
+    km = knor.Kmeans(8)
+    return km.fit(FN, 50, 5)
 
 def dexm_test_c_im():
-    centers = np.random.random((2,5))
-    return knor.Kmeans(FN, centers, nrow=50, ncol=5)
+    k = 2
+    centers = np.random.random((k,5)).flatten()
+    km = knor.Kmeans(k, centers=centers)
+    return km.fit(FN, 50, 5)
 
 def test_err():
     try:
-        knor.Kmeans(FN, 7.2, nrow=50, ncol=5)
+        km = knor.Kmeans(7.2)
+        return  km.fit(FN, nrow=50, ncol=5)
     except Exception as msg:
         print(("Successful test: CORRECTLY fails with message: {}".format(msg)))
 
